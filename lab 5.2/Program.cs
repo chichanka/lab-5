e5eaf538402aba2bb878489579b51fa863f037dd
chichanka <147709699+chichanka@users.noreply.github.com>
2023-11-22 19:07:31 +0200

    Add files via upload

---
+++ lab 5.2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+class MathOperations
+{
+    // Перевантажений метод додавання двох чисел
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Перевантажений метод додавання двох масивів чисел
+    public static int[] Add(int[] array1, int[] array2)
+    {
+        if (array1.Length != array2.Length)
+        {
+            throw new ArgumentException("Arrays must have the same length.");
+        }
+
+        int[] result = new int[array1.Length];
+        for (int i = 0; i < array1.Length; i++)
+        {
+            result[i] = array1[i] + array2[i];
+        }
+
+        return result;
+    }
+
+    // Перевантажений метод додавання двох матриць
+    public static int[,] Add(int[,] matrix1, int[,] matrix2)
+    {
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        {
+            throw new ArgumentException("Matrices must have the same dimensions.");
+        }
+
+        int rows = matrix1.GetLength(0);
+        int columns = matrix1.GetLength(1);
+
+        int[,] result = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+
+    // Перевантажений метод додавання двох тензорів
+    public static int[,,] Add(int[,,] tensor1, int[,,] tensor2)
+    {
+        if (tensor1.GetLength(0) != tensor2.GetLength(0) ||
+            tensor1.GetLength(1) != tensor2.GetLength(1) ||
+            tensor1.GetLength(2) != tensor2.GetLength(2))
+        {
+            throw new ArgumentException("Tensors must have the same dimensions.");
+        }
+
+        int depth = tensor1.GetLength(0);
+        int rows = tensor1.GetLength(1);
+        int columns = tensor1.GetLength(2);
+        int[,,] result = new int[depth, rows, columns];
+        for (int i = 0; i < depth; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                for (int k = 0; k < columns; k++)
+                {
+                    result[i, j, k] = tensor1[i, j, k] + tensor2[i, j, k];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // Додайте інші методи інших операцій, наприклад, віднімання, множення, тощо.
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Приклад використання
+        Console.WriteLine(MathOperations.Add(5, 10));
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 4, 5, 6 };
+        int[] resultArray = MathOperations.Add(array1, array2);
+        Console.WriteLine(string.Join(", ", resultArray));
+        int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+        int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+        int[,] resultMatrix = MathOperations.Add(matrix1, matrix2);
+        Console.WriteLine($"{resultMatrix[0, 0]}, {resultMatrix[0, 1]}, {resultMatrix[1, 0]}, {resultMatrix[1, 1]}");
+    }
+}
+
---
+++ lab_5/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Абстрактний клас транспортних засобів
+abstract class Vehicle
+{
+    public int Speed { get; set; }
+    public int Capacity { get; set; }
+
+    public abstract void Move();
+}
+
+// Клас людини
+class Human
+{
+    public int Speed { get; set; }
+
+    public void Move()
+    {
+        Console.WriteLine("Human is moving.");
+    }
+}
+
+// Спадкоємці класу Vehicle: Car, Bus, Train
+class Car : Vehicle
+{
+    public Car()
+    {
+        Speed = 60;
+        Capacity = 5;
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Car is moving.");
+    }
+}
+
+class Bus : Vehicle
+{
+    public Bus()
+    {
+        Speed = 40;
+        Capacity = 20;
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Bus is moving.");
+    }
+}
+
+class Train : Vehicle
+{
+    public Train()
+    {
+        Speed = 80;
+        Capacity = 100;
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Train is moving.");
+    }
+}
+
+// Клас мережі транспорту
+class TransportNetwork
+{
+    private List<Vehicle> vehicles;
+
+    public TransportNetwork()
+    {
+        vehicles = new List<Vehicle>();
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void ControlMovement()
+    {
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Move();
+        }
+    }
+}
+
+// Клас маршруту
+class Route
+{
+    public string StartPoint { get; set; }
+    public string EndPoint { get; set; }
+
+    public Route(string startPoint, string endPoint)
+    {
+        StartPoint = startPoint;
+        EndPoint = endPoint;
+    }
+
+    public string CalculateOptimalRoute(string vehicleType)
+    {
+        // Реалізація розрахунку оптимального маршруту для конкретного типу транспорту
+        return $"Optimal route for {vehicleType} from {StartPoint} to {EndPoint}.";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Створення об'єктів
+        Car car = new Car();
+        Bus bus = new Bus();
+        Train train = new Train();
+        // Додавання транспортних засобів до мережі
+        TransportNetwork transportNetwork = new TransportNetwork();
+        transportNetwork.AddVehicle(car);
+        transportNetwork.AddVehicle(bus);
+        transportNetwork.AddVehicle(train);
+        // Контроль руху транспорту
+        transportNetwork.ControlMovement();
+        // Створення маршруту
+        Route route = new Route("City A", "City B");
+        // Розрахунок оптимального маршруту для конкретного транспорту
+        string carRoute = route.CalculateOptimalRoute("Car");
+        string busRoute = route.CalculateOptimalRoute("Bus");
+        string trainRoute = route.CalculateOptimalRoute("Train");
+        // Виведення результатів
+        Console.WriteLine(carRoute);
+        Console.WriteLine(busRoute);
+        Console.WriteLine(trainRoute);
+    }
+}
+
